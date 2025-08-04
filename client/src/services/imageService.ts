a6c979945945b693307ed6c9e7f1f621039871fc
MollyMiao <peiyaoshi0321@gmail.com>
2025-08-03 22:34:27 -0400

    new test image

--- client/src/services/imageService.ts
+++ client/src/services/imageService.ts
@@ -1,10 +1,12 @@
 // src/services/imageService.ts
 import TheWeekndImage from '../assets/the-weeknd.png';
+import PostMaloneImage from '../assets/post-malone.png';
 // Import other artist images here
 // e.g. import OliviaRodrigoImage from '../assets/olivia-rodrigo.png';
 
 const artistImages: { [key: string]: string } = {
   'The Weeknd': TheWeekndImage,
+  'Post Malone': PostMaloneImage,
   // 'Olivia Rodrigo': OliviaRodrigoImage,
   // Add other artists and their imported images here
 };
