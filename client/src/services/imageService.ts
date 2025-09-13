8a634c0fa4672b939c430cbd8f077bf60f7021d8
MollyMiao <peiyaoshi0321@gmail.com>
2025-09-13 12:59:22 -0400

    use reduced version

--- client/src/services/imageService.ts
+++ client/src/services/imageService.ts
@@ -1,10 +1,10 @@
 // src/services/imageService.ts
-import TheWeekndImage from '../assets/the-weeknd.png';
-import PostMaloneImage from '../assets/post-malone.png';
-import BillieEilishImage from '../assets/billie-eilish.png';
-import IUImage from '../assets/iu.png';
-import LEImage from '../assets/lesserafim.png';
-import TaylorImage from '../assets/taylorswift.png';
+import TheWeekndImage from '../assets/the-weeknd-min.png';
+import PostMaloneImage from '../assets/post-malone-min.png';
+import BillieEilishImage from '../assets/billie-eilish-min.png';
+import IUImage from '../assets/iu-min.png';
+import LEImage from '../assets/lesserafim-min.png';
+import TaylorImage from '../assets/taylorswift-min.png';
 // Import other artist images here
 // e.g. import OliviaRodrigoImage from '../assets/olivia-rodrigo.png';
 
