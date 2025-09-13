8d65a2b162f94df2a9e4d70813e96757d37e9b0e
MollyMiao <peiyaoshi0321@gmail.com>
2025-09-13 13:22:30 -0400

    imgs

--- client/src/services/imageService.ts
+++ client/src/services/imageService.ts
@@ -5,6 +5,9 @@ import BillieEilishImage from '../assets/billie-eilish-min.png';
 import IUImage from '../assets/iu-min.png';
 import LEImage from '../assets/lesserafim-min.png';
 import TaylorImage from '../assets/taylorswift-min.png';
+import BlackpinkImage from '../assets/blackpink.jpg';
+import KatyImage from '../assets/katy-perry-min.png';
+import SabImage from '../assets/sabrina-carpenter-min.png'
 // Import other artist images here
 // e.g. import OliviaRodrigoImage from '../assets/olivia-rodrigo.png';
 
@@ -15,6 +18,9 @@ const artistImages: { [key: string]: string } = {
   'IU': IUImage,
   'LE SSERAFIM': LEImage,
   'Taylor Swift': TaylorImage,
+  'Blackpink': BlackpinkImage,
+  'Katy Perry': KatyImage,
+  'Sabrina Carpenter': SabImage,
   // 'Olivia Rodrigo': OliviaRodrigoImage,
   // Add other artists and their imported images here
 };
